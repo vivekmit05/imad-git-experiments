f8ef303a04ea5a5b9e2abed7b47751ccb127008e
vivekmit05 <vivek.mit05@gmail.com>
2017-09-20 00:47:03 +0530

    New feautres added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+function newFeature(){
+	console.log('Developing some cool new features');
+}
 function random(){
 	console.log('Inside a function');
 }
 console.log('Hello');
 random();
+newFeature();
