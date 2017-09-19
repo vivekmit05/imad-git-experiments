376896919551746b351629a4ca44e5f3ddb6ae7a
vivekmit05 <vivek.mit05@gmail.com>
2017-09-20 01:08:00 +0530

    New Feature developed successfully

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function newFeature(){
 	console.log('Developing some cool new features');
+	console.log('New feauture developed successfully');
 }
 function random(){
 	console.log('Inside a function');
